8bd004df90a6fa88652e812152461a458d0643a2
Thomas Stokes <gitlab.pibkj@simplelogin.com>
2024-01-05 08:23:28 -0700

    Add in movie loader and model

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# DATA DIRECTORY
+DATA_DIR = BASE_DIR / "data"
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles',
+    'movies'
 ]
 
 MIDDLEWARE = [
--- src/profiles/management/commands/loader.py
+++ src/profiles/management/commands/loader.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from cfehome import utils as cfehome_utils
+from movies.models import Movie
 
 User = get_user_model()
 
@@ -14,24 +15,47 @@ class Command(BaseCommand):
             action='store_true',
             default=False
         )
+        parser.add_argument(
+            '--movies',
+            action='store_true',
+            default=False
+        )
+        parser.add_argument(
+            '--users',
+            action='store_true',
+            default=False
+        )
 
     def handle(self, *args, **options):
         count = options.get('count')
         show_total = options.get('show_total')
-        profiles = cfehome_utils.get_fake_profiles(
-            count=count
-        )
-        new_users = []
-
-        for profile in profiles:
-            new_users.append(
-                User(**profile)
+        load_movies = options.get('movies')
+        generate_users = options.get('users')
+        if load_movies:
+            movie_dataset = cfehome_utils.load_movie_data(limit=count)
+            movies_new = [Movie(**x) for x in movie_dataset]
+            movies_bulk = Movie.objects.bulk_create(
+                movies_new,
+                ignore_conflicts=True
             )
+            print(f"New movies: {len(movies_bulk)}")
+            if show_total:
+                print(f"Total movies: {Movie.objects.count()}")
+        if generate_users:
+            profiles = cfehome_utils.get_fake_profiles(
+                count=count
+            )
+            new_users = []
 
-        users_bulk = User.objects.bulk_create(
-            new_users,
-            ignore_conflicts=True
-        )
-        print(f"New users: {len(users_bulk)}")
-        if show_total:
-            print(f"Total users: {User.objects.count()}")
+            for profile in profiles:
+                new_users.append(
+                    User(**profile)
+                )
+
+            users_bulk = User.objects.bulk_create(
+                new_users,
+                ignore_conflicts=True
+            )
+            print(f"New users: {len(users_bulk)}")
+            if show_total:
+                print(f"Total users: {User.objects.count()}")
