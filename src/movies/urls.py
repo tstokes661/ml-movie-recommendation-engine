e19359c5b9b30ba4a4eb4a1b4e36ed8697166e04
Thomas Stokes <gitlab.pibkj@simplelogin.com>
2024-01-12 16:21:11 -0700

    Add infinite ratings

--- src/movies/urls.py
+++ src/movies/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.movie_list_view),
+    path('infinite/', views.movie_infinite_rating_view),
     path('<int:pk>/', views.movie_detail_view),
 ]
--- src/ratings/views.py
+++ src/ratings/views.py
@@ -11,6 +11,10 @@ def rate_movie_view(request):
         return HttpResponse("Not Allowed", status=400)
     object_id = request.POST.get('object_id')
     rating_value = request.POST.get('rating_value')
+    if object_id is None or rating_value is None:
+        response = HttpResponse("Skipping", status=200)
+        response['HX-Trigger'] = 'did-skip-movie'
+        return response
     user = request.user
     message = "You must login <a href='/accounts/login'>login</a> to rate this"
     if user.is_authenticated:
@@ -26,4 +30,7 @@ def rate_movie_view(request):
         if rating_obj.content_object is not None:
             message = """<span class='bg-success text-light px-3 py-1 rounded'>
             Rating saved</span>"""
+            response = HttpResponse(message, status=200)
+            response['HX-Trigger-After-Settle'] = 'did-rate-movie'
+            return response
     return HttpResponse(message, status=200)
