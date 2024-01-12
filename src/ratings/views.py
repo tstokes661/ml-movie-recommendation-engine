9fe649b2e465874e040730865ceffa61908a5f44
Thomas Stokes <gitlab.pibkj@simplelogin.com>
2024-01-12 15:07:29 -0700

    Ability to rate movies with htmx

--- src/ratings/views.py
+++ src/ratings/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
+from .models import Rating
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+
+@require_http_methods(['POST'])
+def rate_movie_view(request):
+    print(request.POST)
+    if not request.htmx:
+        return HttpResponse("Not Allowed", status=400)
+    object_id = request.POST.get('object_id')
+    rating_value = request.POST.get('rating_value')
+    user = request.user
+    message = "You must login <a href='/accounts/login'>login</a> to rate this"
+    if user.is_authenticated:
+        message = """<span class='bg-danger text-light px-3 py-1 rounded'>
+        An error occured.</span>"""
+        ctype = ContentType.objects.get(app_label='movies', model='movie')
+        rating_obj = Rating.objects.create(
+            content_type=ctype,
+            object_id=object_id,
+            value=rating_value,
+            user=user
+        )
+        if rating_obj.content_object is not None:
+            message = """<span class='bg-success text-light px-3 py-1 rounded'>
+            Rating saved</span>"""
+    return HttpResponse(message, status=200)
